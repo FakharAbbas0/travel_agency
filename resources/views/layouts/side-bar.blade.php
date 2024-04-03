017fc55642c65e6e7be61421691e1ae96886601a
Fakhar Abbas <fakhar.abbas@ozoneddigital.com>
2024-04-03 14:23:23 +0500

    side bar changes

--- resources/views/layouts/side-bar.blade.php
+++ resources/views/layouts/side-bar.blade.php
@@ -2,6 +2,36 @@
 
     <ul class="sidebar-nav" id="sidebar-nav">
 
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{route('admin.dashboard')}}">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{route('admin.invite_colleage')}}">
+                <i class="bi bi-file-earmark"></i>
+                <span>Invite Colleague</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{route('admin.get_extra')}}">
+                <i class="bi bi-file-earmark"></i>
+                <span>Create Extra Quote</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{route('admin.trail_page')}}">
+                <i class="bi bi-file-earmark"></i>
+                <span>Trail Page</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{route('admin.blank')}}">
+                <i class="bi bi-file-earmark"></i>
+                <span>Quotes</span>
+            </a>
+        </li>
         @if (Auth::guard('admin')->check())
 
             <li class="nav-item">
